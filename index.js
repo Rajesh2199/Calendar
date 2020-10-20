d79f956eae7afd6b3417bf96bb31c2b1634bcada
baduraje@isu.edu <baduraje@isu.edu>
2020-10-20 01:57:55 -0600

    added for loop to declare past present or future

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
     var currentInfo = currentDayOfWeek + ", " + currentDay;
     $("#currentDay").text(currentInfo);
     $("#currentTime").text(currentTime);
-})
+    var currentHour = moment().hours();
+
+    // for loop for iterating through all hours to declare past , present and future.
+    for ( i = 0; i < 9; i++ ){
+        if ($(".hour").eq(i).data("hour") === currentHour){
+            $("textarea").eq(i).addClass("present");
+        }   else if ($(".hour").eq(i).data("hour") < currentHour){
+            $("textarea").eq(i).addClass("past");
+        } else {
+            $("textarea").eq(i).addClass("future");
+        }
+    }
+    })
+    
